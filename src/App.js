e79f42e7dc56d20069accfd30c7b5a148f725599
hdriqi <ra@evius.id>
2020-06-30 21:15:54 +0700

    add working Login page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,49 @@ import ipfs from './utils/ipfs'
 
 const MODEL_URL = `http://127.0.0.1:8080/models`
 
+const DEFAULT_AVATAR = [
+  {
+    url: 'QmWvmboVv5wgApgwPcVns3HF3FKpgXSezqtkrJoq5xriCH',
+    type: 'ipfs'
+  },
+  {
+    url: 'Qmab3Umre1GmXuSnkh9wxdr4QNSVtV8DBTLQBRPFbJbUUS',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmfBxyRh5RwBAtB85Q6EGQEfNfprCDdz5mNu3HhLWynpo3',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmRSFZvUwWD61wyYeuYC2B9Z8JPxXXHFmTHeGeRFi7Bn7L',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmYBBFGBZV17oJb9HFQNB7EHmhDMo94GBPBdFnQEqSmF94',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmcUhG6UjqUYKydg7KoAmTok4TSwRXhrvCLb5gciSzatig',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmX6mc7wfDza8mDFMDGfmZsUQNZv1LhP8ztEURmHZf1xAF',
+    type: 'ipfs'
+  },
+  {
+    url: 'Qmdqg7bCjiKagvt421qhGdX5A6jXrAFVaUvxY6DwZMzLtE',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmYeGSYCn14ppxnP26D5p9W8NRgPEBqkfkYfoAULCfKhyE',
+    type: 'ipfs'
+  },
+  {
+    url: 'QmQa68qLbW7iSSVNaS8o1rW9PiCYVoAneqn17D4oz6fbGu',
+    type: 'ipfs'
+  },
+]
+
 const AppContext = createContext()
 export const useNear = () => useContext(AppContext)
 
@@ -22,10 +65,22 @@ const App = ({ contract, contractParas, wallet, account }) => {
       await ipfs.init()
       await faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL)
       setLoading(false)
+
       if (wallet.isSignedIn()) {
-        const profile = await contractParas.getUserById({
+        let profile = await contractParas.getUserById({
           id: account.accountId
         })
+        if (!profile) {
+          const avatar = DEFAULT_AVATAR[Math.floor(Math.random() * DEFAULT_AVATAR.length)]
+          try {
+            profile = await near.contract.createUser({
+              imgAvatar: avatar,
+              bio: ''
+            })
+          } catch (err) {
+            console.log(err)
+          }
+        }
         console.log(profile)
       }
       else {
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNear } from '../App'
+import { withRouter } from 'react-router-dom'
 
-const Login = () => {
+const Login = ({ history }) => {
+  const near = useNear()
+
+  useEffect(() => {
+    if (near.wallet.isSignedIn()) {
+      history.replace('/')
+    }
+  }, [])
+
+  const login = async () => {
+    const appTitle = 'Move by Paras'
+    await near.wallet.requestSignIn(
+      'contract-alpha.paras.testnet',
+      appTitle
+    )
+  }
+
   return (
     <div>
-      Login
+      <button className="w-full rounded-md p-2 bg-primary-5 text-white font-semibold" onClick={login}>Login</button>
     </div>
   )
 }
 
-export default Login
+export default withRouter(Login)
