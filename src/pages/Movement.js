8f536eda970a4625802499a7f8b9d70655f69dd5
hdriqi <ra@evius.id>
2020-07-01 15:12:26 +0700

    hide create new post for non user

--- src/pages/Movement.js
+++ src/pages/Movement.js
@@ -7,9 +7,11 @@ import { useParams, Link } from 'react-router-dom'
 import axios from 'axios'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import InfiniteLoader from '../components/InfiniteLoader'
+import { useNear } from '../App'
 
 const Movement = () => {
   const params = useParams()
+  const near = useNear()
   const [memento, setMemento] = useState({})
   const [postList, setPostList] = useState([])
   const [hasMore, setHasMore] = useState(true)
@@ -76,19 +78,23 @@ const Movement = () => {
           }
         </InfiniteScroll>
       </div>
-      <div className="fixed bottom-0 pb-4" style={{
-        left: `50%`,
-        transform: `translateX(-50%)`
-      }}>
-        <div className="m-auto w-16">
-          <Link className="inline-block" to={`/new/post/${memento.id}`}>
-            <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M56 28C56 43.464 43.464 56 28 56C12.536 56 0 43.464 0 28C0 12.536 12.536 0 28 0C43.464 0 56 12.536 56 28Z" fill="#E13128" />
-              <path fillRule="evenodd" clipRule="evenodd" d="M26.5292 38.6667V30.1375H18V26.5292H26.5292V18H30.1375V26.5292H38.6667V30.1375H30.1375V38.6667H26.5292Z" fill="white" />
-            </svg>
-          </Link>
-        </div>
-      </div>
+      {
+        near.me && (
+          <div className="fixed bottom-0 pb-4" style={{
+            left: `50%`,
+            transform: `translateX(-50%)`
+          }}>
+            <div className="m-auto w-16">
+              <Link className="inline-block" to={`/new/post/${memento.id}`}>
+                <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M56 28C56 43.464 43.464 56 28 56C12.536 56 0 43.464 0 28C0 12.536 12.536 0 28 0C43.464 0 56 12.536 56 28Z" fill="#E13128" />
+                  <path fillRule="evenodd" clipRule="evenodd" d="M26.5292 38.6667V30.1375H18V26.5292H26.5292V18H30.1375V26.5292H38.6667V30.1375H30.1375V38.6667H26.5292Z" fill="white" />
+                </svg>
+              </Link>
+            </div>
+          </div>
+        )
+      }
     </div>
   )
 }
